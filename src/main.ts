366be6ef7711a82b0a58ff5b723b0e3904ff7436
Tien Le <146970839+tcle103@users.noreply.github.com>
2025-10-08 06:59:43 +0000

    added petals button (step 1 complete)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,3 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
-document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-`;
+document.body.innerHTML += "<button>🥀</button>";
