d7d77f18731d70daf7a2aa1b848cc7eff228970b
Tien Le <146970839+tcle103@users.noreply.github.com>
2025-10-10 07:28:15 +0000

    refactored counter/petals updating

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,11 @@ const upgradeButton1: HTMLButtonElement = document.createElement("button");
 let count: number = 0;
 let growthRate: number = 0;
 
+function updatePetals(amt: number): void {
+  count += amt;
+  counterDiv.innerHTML = `Petals: ${count}`;
+}
+
 overallDiv.id = "overall";
 document.body.append(overallDiv);
 infoDiv.id = "info";
@@ -17,22 +22,33 @@ infoDiv.className = "items";
 upgradeDiv.className = "items";
 overallDiv.append(infoDiv);
 overallDiv.append(upgradeDiv);
+
 infoDiv.append(counterDiv);
 counterDiv.innerHTML += `Petals: ${count}`;
 
 button.innerText = "🥀";
 infoDiv.append(button);
 button.addEventListener("click", () => {
-  ++count;
-  counterDiv.innerHTML = `Petals: ${count}`;
+  updatePetals(1);
+  if (count >= 10) {
+    upgradeButton1.disabled = false;
+  }
 });
 
 upgradeButton1.innerText = "Invest in a PetalPlucker 3000";
 upgradeDiv.append(upgradeButton1);
+upgradeButton1.addEventListener("click", () => {
+  ++growthRate;
+  upgradeButton1.disabled = true;
+  updatePetals(-10);
+});
+upgradeButton1.disabled = true;
 
 growthRate = 0;
 
 setInterval(() => {
-  ++count;
-  counterDiv.innerHTML = `Petals: ${count}`;
-}, growthRate);
+  updatePetals(1 * growthRate);
+  if (count >= 10) {
+    upgradeButton1.disabled = false;
+  }
+}, 1000);
