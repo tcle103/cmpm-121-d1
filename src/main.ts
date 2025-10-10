dcc12f1493cd11b2c5bbca0899dae5296145f35a
Tien Le <146970839+tcle103@users.noreply.github.com>
2025-10-10 07:01:28 +0000

    changed up page structure for styling

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,21 @@
 import "./style.css";
 
+const overallDiv: HTMLDivElement = document.createElement("div");
+const infoDiv: HTMLDivElement = document.createElement("div");
 const counterDiv: HTMLDivElement = document.createElement("div");
 const button: HTMLButtonElement = document.createElement("button");
 let count: number = 0;
 let growthRate: number = 0;
 
-document.body.append(counterDiv);
+overallDiv.id = "overall";
+document.body.append(overallDiv);
+infoDiv.id = "info";
+overallDiv.append(infoDiv);
+infoDiv.append(counterDiv);
 counterDiv.innerHTML += `Petals: ${count}`;
 
 button.innerText = "🥀";
-document.body.append(button);
+infoDiv.append(button);
 button.addEventListener("click", () => {
   ++count;
   counterDiv.innerHTML = `Petals: ${count}`;
