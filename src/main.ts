8b1285ca833896a9cd739484403cbedec4782fc3
Tien Le <146970839+tcle103@users.noreply.github.com>
2025-10-10 07:13:12 +0000

    css styling

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,21 @@ import "./style.css";
 
 const overallDiv: HTMLDivElement = document.createElement("div");
 const infoDiv: HTMLDivElement = document.createElement("div");
+const upgradeDiv: HTMLDivElement = document.createElement("div");
 const counterDiv: HTMLDivElement = document.createElement("div");
 const button: HTMLButtonElement = document.createElement("button");
+const upgradeButton1: HTMLButtonElement = document.createElement("button");
 let count: number = 0;
 let growthRate: number = 0;
 
 overallDiv.id = "overall";
 document.body.append(overallDiv);
 infoDiv.id = "info";
+upgradeDiv.id = "upgrades";
+infoDiv.className = "items";
+upgradeDiv.className = "items";
 overallDiv.append(infoDiv);
+overallDiv.append(upgradeDiv);
 infoDiv.append(counterDiv);
 counterDiv.innerHTML += `Petals: ${count}`;
 
@@ -20,6 +26,9 @@ button.addEventListener("click", () => {
   ++count;
   counterDiv.innerHTML = `Petals: ${count}`;
 });
+
+upgradeButton1.innerText = "Invest in a PetalPlucker 3000";
+upgradeDiv.append(upgradeButton1);
 
 growthRate = 0;
 
