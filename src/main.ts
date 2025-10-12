a647fcf79033cd79de6d292c9475603408be6ad6
Tien Le <146970839+tcle103@users.noreply.github.com>
2025-10-12 07:40:24 +0000

    turned upgrade price into variables, added price update calculation to button events, formatted string in button text (step 7 complete)

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,9 @@ const button: HTMLButtonElement = document.createElement("button");
 const upgradeButton1: HTMLButtonElement = document.createElement("button");
 const upgradeButton2: HTMLButtonElement = document.createElement("button");
 const upgradeButton3: HTMLButtonElement = document.createElement("button");
+let butt1Price: number = 10;
+let butt2Price: number = 100;
+let butt3Price: number = 1000;
 let count: number = 0;
 let growthRate: number = 0;
 let start: number = performance.now();
@@ -27,17 +30,17 @@ function updatePetals(amt: number): void {
 }
 
 function updateButton(): void {
-  if (count >= 10) {
+  if (count >= butt1Price) {
     upgradeButton1.disabled = false;
   } else {
     upgradeButton1.disabled = true;
   }
-  if (count >= 100) {
+  if (count >= butt2Price) {
     upgradeButton2.disabled = false;
   } else {
     upgradeButton2.disabled = true;
   }
-  if (count >= 1000) {
+  if (count >= butt3Price) {
     upgradeButton3.disabled = false;
   } else {
     upgradeButton3.disabled = true;
@@ -97,38 +100,50 @@ butt2Div.style.display = "none";
 butt3Div.style.display = "none";
 
 upgradeButton1.innerText =
-  "Invest in a PetalPlucker 3000 for 10 petals\n(+0.1 petals/sec)";
+  `Invest in a PetalPlucker 3000 for ${butt1Price} petals\n(+0.1 petals/sec)`;
 upgradeDiv.append(upgradeButton1);
 upgradeButton1.addEventListener("click", () => {
   updateGrowth(.1);
   updateButton();
-  updatePetals(-10);
+  updatePetals(-butt1Price);
   butt1Div.style.display = "block";
   butt1Div.innerHTML = `PetalPluckers: ${++butt1Owned} owned`;
+  butt1Price = butt1Price * 1.15;
+  upgradeButton1.innerText = `Invest in a PetalPlucker 3000 for ${
+    butt1Price.toFixed(1)
+  } petals\n(+0.1 petals/sec)`;
 });
 upgradeButton1.disabled = true;
 
 upgradeButton2.innerText =
-  "Invest in a BloomBuster 250 for 100 petals\n(+2.0 petals/sec)";
+  `Invest in a BloomBuster 250 for ${butt2Price} petals\n(+2.0 petals/sec)`;
 upgradeDiv.append(upgradeButton2);
 upgradeButton2.addEventListener("click", () => {
   updateGrowth(2);
   updateButton();
-  updatePetals(-100);
+  updatePetals(-butt2Price);
   butt2Div.style.display = "block";
   butt2Div.innerHTML = `BloomBusters: ${++butt2Owned} owned`;
+  butt2Price = butt2Price * 1.15;
+  upgradeButton2.innerText = `Invest in a BloomBuster 250 for ${
+    butt2Price.toFixed(1)
+  } petals\n(+2.0 petals/sec)`;
 });
 upgradeButton2.disabled = true;
 
 upgradeButton3.innerText =
-  "Invest in a FlowerMower X for 1000 petals\n(+50 petals/sec)";
+  `Invest in a FlowerMower X for ${butt3Price} petals\n(+50 petals/sec)`;
 upgradeDiv.append(upgradeButton3);
 upgradeButton3.addEventListener("click", () => {
   updateGrowth(50);
   updateButton();
-  updatePetals(-1000);
+  updatePetals(-butt3Price);
   butt3Div.style.display = "block";
   butt3Div.innerHTML = `FlowerMowers: ${++butt3Owned} owned`;
+  butt3Price = butt3Price * 1.15;
+  upgradeButton3.innerText = `Invest in a FlowerMower X for ${
+    butt3Price.toFixed(1)
+  } petals\n(+50 petals/sec)`;
 });
 upgradeButton3.disabled = true;
 
